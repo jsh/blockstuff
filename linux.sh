f0635ad5b088950a9659138f0c704d9fb857fc79
Jeffrey S. Haemer <jeffrey.haemer@gmail.com>
2020-12-29 17:44:06 -0700

    Linux doesn't have 3.9 yet.

--- linux.sh
+++ linux.sh
@@ -9,11 +9,7 @@ sudo apt upgrade
 sudo apt install black ipython3 pypy3 python3-pip
 pip3 install --user pipenv
 PATH+=:~/.local/bin
-sed -i 's/3.9/3.8.5/' Pipfile
-pipenv --rm
-pipenv --python 3.8
-pipenv update
-# pipenv install pypy3
+pipenv --python 3.8 update
 pipenv install black ||
   pipenv lock --pre
 pipenv shell
